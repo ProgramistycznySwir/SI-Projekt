50b673e12d977c83e471f8e085a2b8b28c610ddd
Maciej Sobolewski <macieksob25@gmail.com>
2021-06-14 23:40:39 +0200

    Added moving mouse and one line in Points

--- SingleNeuronVisualisation/MVVM/View/Points.xaml.cs
+++ SingleNeuronVisualisation/MVVM/View/Points.xaml.cs
@@ -24,5 +24,42 @@ namespace SingleNeuronVisualisation.MVVM.View
         {
             InitializeComponent();
         }
+
+        private void ListView_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+
+        private void ButtonGeneratePoints_Click(object sender, RoutedEventArgs e)
+        {
+           
+        }
+
+        Point scrollMousePoint = new Point();
+        double hOff = 1;
+        private void scrollViewer_PreviewMouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            scrollMousePoint = e.GetPosition(scrollViewer);
+            hOff = scrollViewer.HorizontalOffset;
+            scrollViewer.CaptureMouse();
+        }
+
+        private void scrollViewer_PreviewMouseMove(object sender, MouseEventArgs e)
+        {
+            if (scrollViewer.IsMouseCaptured)
+            {
+                scrollViewer.ScrollToHorizontalOffset(hOff + (scrollMousePoint.X - e.GetPosition(scrollViewer).X));
+            }
+        }
+
+        private void scrollViewer_PreviewMouseLeftButtonUp(object sender, MouseButtonEventArgs e)
+        {
+            scrollViewer.ReleaseMouseCapture();
+        }
+
+        private void scrollViewer_PreviewMouseWheel(object sender, MouseWheelEventArgs e)
+        {
+            scrollViewer.ScrollToHorizontalOffset(scrollViewer.HorizontalOffset + e.Delta);
+        }
     }
 }
